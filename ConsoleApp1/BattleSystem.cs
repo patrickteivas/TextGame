4f91b65014ca7bc0f0f824e1f9aba6c503933abb
patrickteivas <teivas.2011@mail.ru>
2017-12-11 12:57:16 +0200

    Commit

--- ConsoleApp1/BattleSystem.cs
+++ ConsoleApp1/BattleSystem.cs
@@ -8,5 +8,11 @@ namespace ConsoleApp1
 {
     class BattleSystem
     {
+        public int Stamina { get; set; }
+        public int Aigility { get; set; }
+        public int Lucky { get; set; }
+        public int Respect { get; set; }
+        public int Money { get; set; }
+        public int Karma { get; set; }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var Artjom = new BattleSystem();
+            Artjom.Money = 100;
+            Artjom.Karma = 0;
+            Artjom.Lucky = 0;
+
+            Console.WriteLine("Sinu tegelane on Artjom, üheksanda klassi Õpilane, kes hiljuti kolis Ida-Virumaale Kohtla-Järvesse. Linnas on tal raske tutvuda inimestega ning tal trenn, mis algas alles paari kuu eest, jäi katki, ning nüüd peab ta otsima uue treeninguala.");
         }
     }
 }
